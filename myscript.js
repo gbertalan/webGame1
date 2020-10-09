4228707057085577c20ca2d5ce5e1cd3bd64cf45
Gergely Bertalan <bertalanofficial@gmail.com>
2020-10-09 14:55:22 +0200

    Make playing-field adopt to window size and remove not used files

--- myscript.js
+++ myscript.js
@@ -7,17 +7,19 @@ div.style = "width:2%; height:2%; border:1px solid black";
 playingField.appendChild(div);
 }
 */
-for (let i = 0; i < 20; ++i) {
-let row = document.createElement("tr");
-row.style = " min-width: 0px; min-height: 0px;";
-for (let i = 0; i < 20; ++i) {
-let cell = document.createElement("td");
-let div = document.createElement("div");
-cell.style = "background-color: green; min-width: 0px; min-height: 0px;";
-div.style = "width:2vh; height:2vh; border:1px solid red; background-color: black; min-width: 0px; min-height: 0px;";
-cell.appendChild(div);
-row.appendChild(cell);
+for (let i = 0; i < 30; ++i) {
+    let row = document.createElement("tr");
+    row.style = "max-width: 100vw;";
+    for (let i = 0; i < 30; ++i) {
+        let cell = document.createElement("td");
+
+        cell.style = "background-color: green; width:2vh; height:2vh; border:1px solid red; background-color: black;";
+        
+        row.appendChild(cell);
+    }
+    playingFieldTable.appendChild(row);
 }
-playingFieldTable.appendChild(row);
-}
+
+let main = document.getElementsByTagName("main");
+main.style = "max-width: 100vw";
 
