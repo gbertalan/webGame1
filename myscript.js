aec0f2ff5b05ae41fea71a66aeeb64041df98399
Gergely Bertalan <bertalanofficial@gmail.com>
2020-10-09 10:56:57 +0200

    Add squares dynamically

--- myscript.js
+++ myscript.js
@@ -2,6 +2,13 @@ let playingField = document.getElementById("playing-field");
 let image = document.getElementById("playing-field-image");
 playingField.removeChild(image);
 
+
+for (let i = 0; i < 140; ++i) {
 let div = document.createElement("div");
 div.style = "width:2vh; height:2vh; border:1px solid black";
 playingField.appendChild(div);
+}
+
+let div2 = document.createElement("div");
+div2.style = "width:2vh; height:2vh; border:1px solid black";
+playingField.appendChild(div2);
